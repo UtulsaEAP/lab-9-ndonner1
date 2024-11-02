70c13e5e6aa233525054f5b7f10652e7a0721631
ndonner1 <npd3070@utulsa.edu>
2024-11-02 15:58:33 -0500

    finished swap values problem?

--- problem 5/varSwap.py
+++ problem 5/varSwap.py
@@ -1,14 +1,22 @@
+# Noah Donnermeyer
+# Th 8am
+
 def swap_values(user_val1, user_val2, user_val3, user_val4):   
    #write your code here
-   user_val1 = user_val2
-   return user_val1, user_val2, user_val3, user_val4
+   tuple_2143 = (user_val2 + " "), (user_val1 + " "), (user_val4 + " "), user_val3
+   final_string = ''
+   for i in tuple_2143:
+      final_string = final_string + i
+   return final_string
 
 if __name__ == '__main__':   
-   user_input1 = int(input())
-   user_input2 = int(input())
-   user_input3 = int(input())
-   user_input4 = int(input())
+   user_input1 = str(int(input()))
+   user_input2 = str(int(input()))
+   user_input3 = str(int(input()))
+   user_input4 = str(int(input()))
    #store output for every input here
+   swap_values(user_input1, user_input2, user_input3, user_input4)
    #print those output
+   print(swap_values(user_input1, user_input2, user_input3, user_input4))
 
  
